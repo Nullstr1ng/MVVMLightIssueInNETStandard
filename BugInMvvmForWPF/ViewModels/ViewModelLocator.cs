04e3e8405ec7ee3450762214c3cccfbcd7443f0a
Jayson Ragasa <jayson.d.ragasa@live.com>
2019-07-29 15:42:21 +0800

    THis branch is using the local ViewModelLocator in WPF.  Which is working

---
+++ BugInMvvmForWPF/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace BugInMvvmForWPF.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<ViewModel_Main>();
+        }
+
+        public ViewModel_Main Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<ViewModel_Main>();
+            }
+        }
+    }
+}
---
+++ BugInMvvmForWPF/ViewModels/ViewModel_Main.cs
@@ -0,0 +1,112 @@
+﻿using BugInMvvmForWPF.Lib.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BugInMvvmForWPF.ViewModels
+{
+    public class ViewModel_Main : ViewModelBase
+    {
+        #region events
+
+        #endregion
+
+        #region vars
+
+        #endregion
+
+        #region properties
+        public ObservableCollection<Model_Person> PersonCollection { get; set; } = new ObservableCollection<Model_Person>();
+        #endregion
+
+        #region commands
+        public ICommand Command_Add { get; set; }
+        // move this command methods
+        #endregion
+
+        #region ctors
+        public ViewModel_Main()
+        {
+            InitCommands();
+
+            // used only in UWP & WPF
+            // or anything that supports design time updates
+            if (base.IsInDesignMode)
+            {
+                DesignData();
+            }
+            else
+            {
+                RuntimeData();
+            }
+        }
+        #endregion
+
+        #region command methods
+        void Command_Add_Click()
+        {
+            this.PersonCollection.Add(new Model_Person()
+            {
+                FirstName = "Person",
+                LastName = "Person's lastname"
+            });
+        }
+        #endregion
+
+        #region methods
+        void InitCommands()
+        {
+            if (Command_Add == null) Command_Add = new RelayCommand(Command_Add_Click);
+        }
+
+        void DesignData()
+        {
+            this.PersonCollection.Add(new Model_Person()
+            {
+                FirstName = "DESIGN_Rodolfo",
+                LastName = "Presbitero"
+            });
+
+            this.PersonCollection.Add(new Model_Person()
+            {
+                FirstName = "DESIGN_Ralph",
+                LastName = "Guiterrez"
+            });
+
+            this.PersonCollection.Add(new Model_Person()
+            {
+                FirstName = "DESIGN_Cy",
+                LastName = "Loleng"
+            });
+        }
+
+        void RuntimeData()
+        {
+            this.PersonCollection.Add(new Model_Person()
+            {
+                FirstName = "Rodolfo",
+                LastName = "Presbitero"
+            });
+
+            this.PersonCollection.Add(new Model_Person()
+            {
+                FirstName = "Ralph",
+                LastName = "Guiterrez"
+            });
+
+            this.PersonCollection.Add(new Model_Person()
+            {
+                FirstName = "Cy",
+                LastName = "Loleng"
+            });
+        }
+
+        public async Task RefreshData()
+        {
+
+        }
+        #endregion
+    }
+}
